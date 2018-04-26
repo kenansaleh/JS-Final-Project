daca39fcc6d9662fba2b808c7381a9740c6d1e64
kenansaleh <38595215+kenansaleh@users.noreply.github.com>
2018-04-26 03:28:03 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,40 +1,62 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var socket = require('socket.io')(server);
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require ('socket.io').listen(server);
 
-server.listen(8080);
+var streams = {};
+var connections = [];
+
+var constraints = window.constraints = {
+	video: true,
+	audio: false
+}
+
+server.listen(process.env.PORT || 3000);
+console.log('Server runnnig on port: 3000');
+
+app.use(express.static('dir'));
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+io.sockets.on('connection', function(socket) {
+	connections.push(socket);
 
-var streams = {} 
-var numStreams = 0;
+	socket.on('disconnect', function (data) {
+		if (!socket.streamName) return;
+		streams.splice(strams.indexOf(socket.streamName), 1)
+		updateStreams();
 
-socket.on('connection', function (socket) {
-
-	socket.emit('current_streams', streams);
-
-	socket.on('create_stream', function (stream) {
-
-		var newStream = {
-			host: socket.in,
-			numViewers: 0,
-			viewers: {},
-			id: numStreams
-		}
-
-		numStreams++;
-
-		streams[newStream.id] = newStream;
-		socket.emit('stream_added', newStream);
+		connections.splice(connections.indexOf(socket, 1));
 	});
 
-	socket.on('enter_stream', function (id, viewer) {
-		if (streams[id]) {
-			socket.join('Stream ' + id);
-			socket.to('Stream ' + id).emit('viewer_joined', viewer);
-		}
+	socket.on('send comment', function (data) {
+		io.sockets.emit('new comment', {comment: data});
 	})
+
+	socket.on('new stream', function (data, cb) {
+		callback(true);
+		socket.streamName = data;
+		users.push(socket.streamName);
+		updateStreams();
+	})
+
+	function updateStreams() {
+		io.sockets.emit('get streams', streams)
+	}
+
+	function handleSuccess(stream) {
+      window.stream = stream;
+      io.sockets.emit('broadcast stream', stream)
+      localVideo.srcObject = stream;
+    }
+
+	function errorStream (error) {
+      console.log('getUserMedia error: ' + error.name, error);
+    }
+
+	function beginStream {
+		navigator.mediaDevices.getUserMedia(constraints).then(successStream).catch(errorStream);
+	}
 });
